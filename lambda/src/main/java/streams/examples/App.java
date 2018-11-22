6e2bf1f6d08121aed27ae0deedaac518a36d346f
Ida Amit <Ida.Amit@nice.com>
2018-11-22 20:28:50 +0200

    small changes

--- lambda/src/main/java/streams/examples/App.java
+++ lambda/src/main/java/streams/examples/App.java
@@ -1,4 +1,4 @@
-package main.java.streams.examples;
+package streams.examples;
 
 import java.util.Arrays;
 import java.util.List;
--- lambda/src/main/java/streams/examples/TriPredicate.java
+++ lambda/src/main/java/streams/examples/TriPredicate.java
@@ -1,4 +1,4 @@
-package main.java.streams.examples;
+package streams.examples;
 
 
 @FunctionalInterface
